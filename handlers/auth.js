c3b6521388be006e6ea8c8dc30a855f7ac74b85a
Nick Santoro <santoro.nick@gmail.com>
2019-02-16 17:53:46 -0800

    fixed bugs with login and messages, updated code

--- handlers/auth.js
+++ handlers/auth.js
@@ -6,8 +6,8 @@ exports.signin = async function (req, res, next) {
     let user = await db.User.findOne({
       email: req.body.email
     });
-    let { id, username, profileImageUrl } = user
-    let isMatch = await user.comparePassword(req.body.password)
+    let { id, username, profileImageUrl } = user;
+    let isMatch = await user.comparePassword(req.body.password);
     if (isMatch) {
       let token = jwt.sign(
         {
@@ -15,19 +15,19 @@ exports.signin = async function (req, res, next) {
           username,
           profileImageUrl
         },
-        proccess.env.SECRET_KEY
+        process.env.SECRET_KEY
       );
       return res.status(200).json({
         id,
         username,
         profileImageUrl,
         token
-      })
+      });
     } else {
       return next({
         status: 400,
         message: "Invalid Email or Password"
-      })
+      });
     }
   } catch (e) {
     return next({ status: 400, message: "Invalid Email or Password" })
@@ -38,7 +38,7 @@ exports.signup = async function (req, res, next) {
   try {
     // create user
     let user = await db.User.create(req.body);
-    let { id, username, profileImageUrl } = user
+    let { id, username, profileImageUrl } = user;
     // create a token (signing a token)
     let token = jwt.sign(
       {
@@ -54,7 +54,7 @@ exports.signup = async function (req, res, next) {
       username,
       profileImageUrl,
       token
-    })
+    });
   } catch (err) {
     // if a validation fails
     if (err.code === 11000) {
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,21 +6,15 @@ const jwt = require("jsonwebtoken");
 exports.loginRequired = function (req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET.KEY, function (err, decoded) {
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
       if (decoded) {
         return next();
       } else {
-        return next({
-          status: 401,
-          message: "Please log in first"
-        });
+        return next({ status: 401, message: "Please log in first" });
       }
-    })
+    });
   } catch (e) {
-    return next({
-      status: 401,
-      message: "Please log in first"
-    })
+    return next({ status: 401, message: "Please log in first" });
   }
 };
 
@@ -34,17 +28,11 @@ exports.ensureCorrectUser = function (req, res, next) {
       if (decoded && decoded.id === req.params.id) {
         return next();
       } else {
-        return next({
-          status: 401,
-          message: "Unauthorized"
-        });
+        return next({ status: 401, message: "Unauthorized" });
       }
     });
   } catch (e) {
-    return next({
-      status: 401,
-      message: "Unauthorized"
-    })
+    return next({ status: 401, message: "Unauthorized" })
   }
 };
 
